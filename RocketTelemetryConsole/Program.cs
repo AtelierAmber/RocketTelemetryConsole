7a2e3410f50a75de59cf883544a7a908e3b4c074
Amber <aqscode@gmail.com>
2023-12-28 16:32:05 -0700

    Initial Commit

---
+++ RocketTelemetryConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using SadConsole;
+using SadConsole.Configuration;
+
+namespace RocketTelemetryConsole
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      SadConsole.Settings.WindowTitle = "Telemetry Console";
+
+      SadConsole.Settings.AllowWindowResize = false;
+      SadConsole.Settings.ResizeMode = SadConsole.Settings.WindowResizeOptions.None;
+
+      Builder startup = new Builder()
+        .IsStartingScreenFocused(false)
+        .ConfigureFonts("./Fonts/Cheepicus12.font")
+        ;
+
+      SadConsole.Game.Create(startup);
+      SadConsole.Game.Instance.Started += Init;
+      SadConsole.Game.Instance.Run();
+      SadConsole.Game.Instance.Dispose();
+    }
+
+    public static void Init(object? sender, SadConsole.GameHost host)
+    {
+      //SadConsole.Game.Instance.ToggleFullScreen();
+    }
+  }
+}
